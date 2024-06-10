fb8a65671f6a4977c71c050d02476aa9206cc44a
lemonwasp <30548917+lemonwasp@users.noreply.github.com>
2024-06-10 15:55:10 +0900

    Update IntroducePage.jsx

--- 2024년1학기시스템구축기초 기말과제-2조/src/Introduce/IntroducePage.jsx
+++ 2024년1학기시스템구축기초 기말과제-2조/src/Introduce/IntroducePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './IntroducePage.css';
 import Modal from 'react-modal';
 import AddInfo from './AddInfo';
 import UseFetch from './UseFetch';
@@ -177,4 +177,4 @@ function App() {
   );
 }
 
-export default App;
+export default IntroducePage;
