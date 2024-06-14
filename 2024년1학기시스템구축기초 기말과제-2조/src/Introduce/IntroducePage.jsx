1640d6618961706467742f29949371ccba06d3af
lemonwasp <30548917+lemonwasp@users.noreply.github.com>
2024-06-14 11:44:34 +0900

    Update IntroducePage.jsx_final

--- 2024년1학기시스템구축기초 기말과제-2조/src/Introduce/IntroducePage.jsx
+++ 2024년1학기시스템구축기초 기말과제-2조/src/Introduce/IntroducePage.jsx
@@ -7,11 +7,13 @@ import UseFetch from './UseFetch';
 Modal.setAppElement('#root');
 
 function IntroducePage() {
+  // json server에 연결
   const TestData = UseFetch('http://localhost:3001/post');
 
   const [activeBox, setActiveBox] = useState(null);
   const [boxes, setBoxes] = useState([]);
 
+  // db.json의 post에 들어있는 값이 변하면 재렌더링
   useEffect(() => {
     if (TestData !== undefined && TestData !== null && TestData.length > 0) {
       const formattedData = TestData.map((item) => ({
@@ -24,12 +26,14 @@ function IntroducePage() {
     }
   }, [TestData]);
 
+  // 모달 CRUD 관련 useState 선언
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [infoModalIsOpen, setInfoModalIsOpen] = useState(false);
   const [boxToDelete, setBoxToDelete] = useState(null);
   const [boxToEdit, setBoxToEdit] = useState(null);
   const [editModalIsOpen, setEditModalIsOpen] = useState(false);
 
+  // 전체 container의 크기를 설정
   useEffect(() => {
     const updateWidth = () => {
       const containerMaxWidth = window.innerWidth * 0.85;
@@ -44,31 +48,40 @@ function IntroducePage() {
     return () => window.removeEventListener('resize', updateWidth);
   }, [boxes.length]);
 
+  // 마우스 이벤트
+  // 1) 마우스 over상태
   const handleMouseOver = (index) => {
     setActiveBox(index);
   };
-
+  // 2) 마우스 out상태
   const handleMouseOut = () => {
     setActiveBox(null);
   };
 
+
+  // 삭제시 모달 열림
   const openModal = (index) => {
     setBoxToDelete(index);
     setModalIsOpen(true);
   };
-
+  // 모달 닫기
   const closeModal = () => {
     setModalIsOpen(false);
   };
 
+
+  // 삭제 기능
   const deleteBox = async () => {
     try {
+      // json server에 연결 - 삭제 모드로
       const response = await fetch(`http://localhost:3001/post/${boxes[boxToDelete].id}`, {
         method: 'DELETE',
       });
+      // 연결 불안정하면 알림
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
+      // 삭제할 박스와 인덱스 값이 같으면 삭제
       setBoxes((prevBoxes) => prevBoxes.filter((_, i) => i !== boxToDelete));
       setBoxToDelete(null);
       setModalIsOpen(false);
@@ -76,29 +89,34 @@ function IntroducePage() {
       console.error(`Error deleting box: ${error}`);
     }
   };
+  
 
+  //<추가>
+  // 박스 추가할 때 모달 열기
   const addBox = () => {
     setInfoModalIsOpen(true);
   };
-
-  const closeInfoModal = () => {
-    setInfoModalIsOpen(false);
-  };
-
+  // 추가 후 닫기
   const addBoxInfo = (info) => {
     setBoxes((prevBoxes) => [...prevBoxes, info]);
     closeInfoModal();
   };
+  // 박스 닫기(추가 안하고)
+  const closeInfoModal = () => {
+    setInfoModalIsOpen(false);
+  };
 
+
+  // <수정>
+  // 수정 모달 열기/닫기
   const openEditModal = (index) => {
     setBoxToEdit(index);
     setEditModalIsOpen(true);
   };
-
   const closeEditModal = () => {
     setEditModalIsOpen(false);
   };
-
+  // 수정할 박스의 인덱스와 같으면 수정할 값으로, 아니면 원래 값으로 하고 닫기
   const updateBoxInfo = (updatedInfo) => {
     setBoxes((prevBoxes) => prevBoxes.map((box, index) => (index === boxToEdit ? updatedInfo : box)));
     closeEditModal();
@@ -151,7 +169,6 @@ function IntroducePage() {
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        contentLabel="Delete Box Confirmation"
         className="modal"
         overlayClassName="overlay-modal"
       >
@@ -162,7 +179,6 @@ function IntroducePage() {
       <Modal
         isOpen={infoModalIsOpen}
         onRequestClose={closeInfoModal}
-        contentLabel="Add Box Information"
         className="modal"
         overlayClassName="overlay-modal"
       >
@@ -171,7 +187,6 @@ function IntroducePage() {
       <Modal
         isOpen={editModalIsOpen}
         onRequestClose={closeEditModal}
-        contentLabel="Edit Box Information"
         className="modal"
         overlayClassName="overlay-modal"
       >
