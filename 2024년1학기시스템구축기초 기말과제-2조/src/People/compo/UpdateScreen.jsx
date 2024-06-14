b123a532daf7f008b305c82210c3fac2dee1839f
dlat.th <dlat.th45@gmail.com>
2024-06-14 14:29:12 +0900

    people_css & DB

--- 2024년1학기시스템구축기초 기말과제-2조/src/People/compo/UpdateScreen.jsx
+++ 2024년1학기시스템구축기초 기말과제-2조/src/People/compo/UpdateScreen.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const UpdateScreen = (props) => {
-
   const del = () => {
     const dellist = props.user;
     props.onOk();
@@ -14,7 +13,9 @@ const UpdateScreen = (props) => {
       <div className="P-modal-wrap">
         <header>
           <h2>{props.title}</h2>
-          <button id="P-x-btn" onClick={props.onClose}>X</button>
+          <button id="P-x-btn" onClick={props.onClose}>
+            X
+          </button>
         </header>
         <div className="content">
           <form
@@ -30,21 +31,53 @@ const UpdateScreen = (props) => {
               props.onOk();
             }}
           >
-            <div>Img<div><textarea name='img' defaultValue={props.user.img}></textarea></div></div>
-            <div>Name<div><textarea name='name' defaultValue={props.user.name}></textarea></div></div>
-            <div>Skill<div><textarea name='skill' defaultValue={props.user.skill}></textarea></div></div>
-            <div>Experience<div><textarea name='experience' defaultValue={props.user.experience}></textarea></div></div>
-            <div>Contact<div><textarea name='contact' defaultValue={props.user.contact}></textarea></div></div>
-            <div>Hobbie<div><textarea name='hobbie' defaultValue={props.user.hobbie}></textarea></div></div>
+            <div>
+              Img
+              <div>
+                <textarea name="img" defaultValue={props.user.img}></textarea>
+              </div>
+            </div>
+            <div>
+              Name
+              <div>
+                <textarea name="name" defaultValue={props.user.name}></textarea>
+              </div>
+            </div>
+            <div>
+              Skill
+              <div>
+                <textarea name="skill" defaultValue={props.user.skill}></textarea>
+              </div>
+            </div>
+            <div>
+              Experience
+              <div>
+                <textarea name="experience" defaultValue={props.user.experience}></textarea>
+              </div>
+            </div>
+            <div>
+              Contact
+              <div>
+                <textarea name="contact" defaultValue={props.user.contact}></textarea>
+              </div>
+            </div>
+            <div>
+              Hobbie
+              <div>
+                <textarea name="hobbie" defaultValue={props.user.hobbie}></textarea>
+              </div>
+            </div>
             <footer>
-              <button type='submit'>수정</button>
-              <button type='button' onClick={del}>삭제</button>
+              <button type="submit">수정</button>
+              <button type="button" onClick={del}>
+                삭제
+              </button>
             </footer>
           </form>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default UpdateScreen;
